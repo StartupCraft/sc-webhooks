5d55d274642c7f85ee54a419009f2bdb88161472
Vadim Shalamov <vadshalamov@gmail.com>
2018-08-15 13:34:07 +0500

    remove append_migrations

--- lib/sc/webhooks/engine.rb
+++ lib/sc/webhooks/engine.rb
@@ -5,10 +5,6 @@ module SC
     class Engine < ::Rails::Engine
       isolate_namespace SC::Webhooks
 
-      initializer :append_migrations do |app|
-        app.config.paths['db/migrate'] += config.paths['db/migrate'].expanded unless app.root.to_s.match?(root.to_s)
-      end
-
       config.generators do |g|
         g.test_framework :rspec
         g.fixture_replacement :factory_bot, dir: 'spec/factories'
