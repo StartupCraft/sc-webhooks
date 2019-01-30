a44a4af63f9dc198dcfe9951aa766af070c600c6
Vadim Shalamov <vadshalamov@gmail.com>
2019-01-31 00:36:37 +0500

    add possibility to customize responses

--- spec/dummy/config/initializers/sc-webhooks.rb
+++ spec/dummy/config/initializers/sc-webhooks.rb
@@ -2,6 +2,19 @@
 
 SC::Webhooks.configure do |config|
   config.handlers = {
-    'configured_webhook' => ::Webhooks::ConfiguredWebhookAction
+    'configured_webhook' => ::Webhooks::ConfiguredWebhookAction,
+    'webhook_with_custom_response' => {
+      action_class: ::Webhooks::ConfiguredWebhookAction,
+      response_handler: ->(context, monad) do
+        monad.success do
+          context.status = 200
+          context.response_body = 'Hey there!'
+        end
+
+        monad.failure do
+          context.status = 400
+        end
+      end
+    }
   }
 end
